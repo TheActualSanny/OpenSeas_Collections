f2258a519a1b1add1c5ee130a999d29c817af985
Sanny <dugna1234@gmail.com>
2024-05-07 03:06:38 +0400

    Revert "Updated the main, It uses the ORM now instead of the database_manager"
    This reverts commit c866d3d46f3e38e90529f88cc0447b60a65387b5.

--- main.py
+++ main.py
@@ -2,22 +2,18 @@
 from extract_collections import Extract_Collections
 from extract_item import Extract_ColItems
 from extract_owners import Extract_Owners
-from custom_orm import Cust_ORM
+from database_manager import Database_Manager
 
 def main():
     '''The main function. The whole functionality is connected here'''
     cols = Extract_Collections()
     items = Extract_ColItems()
     owners = Extract_Owners()
-    orm = Cust_ORM()
-    orm.create_table(table_name = 'collection_items',  id = 'SERIAL PRIMARY KEY', collection_name = 'TEXT',  name = 'TEXT', description = 'TEXT', image_url = 'TEXT',
-                    owner_name = 'TEXT', twitter_username = 'TEXT', contract_address = 'TEXT', contract_chain = 'TEXT') 
-                    
-    orm.select_table('collection_items')
+    manager = Database_Manager()
     collections = cols.get_all_pages()
     owner_data = owners.get_owners(collections)
     nft_data = items.get_nfts(owner_data)
-    orm.insert_rows(nft_data, has_id = True)
+    manager.insert_data(nft_data)
 
 if __name__ == '__main__':
     main() 
